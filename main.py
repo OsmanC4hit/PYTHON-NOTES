34c89fa35d58f11e0451778b84fbffdbc32806bc
lendroy4200 <71992282+lendroy4200@users.noreply.github.com>
2021-12-21 20:09:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+musteri_isim = "osman cahit"
+print("musteri_isim")
+
+musteri_soyisim = "Yüksel"
+print("musteri_soyisim")
+
+print(musteri_isim+musteri_soyisim)
+
+musteri_cinsiyet = "erkek"
+print(musteri_cinsiyet)
+
+musteri_tc = "1234567890"
+print(musteri_tc)
+
+musteri_doğum_yılı = "1990"
+print(musteri_doğum_yılı)
