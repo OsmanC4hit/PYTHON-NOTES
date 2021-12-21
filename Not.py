c1e400f60d90d10bd3935f224fd9ce8fc8e3aa72
lendroy4200 <71992282+lendroy4200@users.noreply.github.com>
2021-12-21 20:05:59 +0300

    Add files via upload

---
+++ Not.py
@@ -0,0 +1,23 @@
+ogrenci1 = "osman"
+ogrenci2 = "cahit"
+ogrenci3 = "ali"
+print("++++++++++++++++++")
+puan = int(input(ogrenci1 + "'in notunu giriniz: "))
+if(puan == 50 or puan > 50):
+    print("Tebrikler dersi gectin")
+elif(puan < 50):
+    print("calisip dersini gecmen lazim")
+print("++++++++++++++++++")
+puan = int(input(ogrenci2 + "'in notunu giriniz: "))
+if(puan == 50 or puan > 50):
+    print("Tebrikler dersi gectin")
+elif(puan < 50):
+    print("calisip dersini gecmen lazim") 
+print("++++++++++++++++++")
+puan = int(input(ogrenci3 + "'in notunu giriniz: "))
+if(puan == 50 or puan > 50):
+    print("Tebrikler dersi gectin")
+elif(puan < 50):
+    print("calisip dersini gecmen lazim")
+print("++++++++++++++++++")
+
