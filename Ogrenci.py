b391fa6e5dc7ee5cd0091058c03c21a3898d7c59
lendroy4200 <71992282+lendroy4200@users.noreply.github.com>
2021-12-21 20:03:00 +0300

    Add files via upload

---
+++ Ogrenci.py
@@ -0,0 +1,13 @@
+studenta = " Osman , 2000 (68,80,70) "
+studentb = " Cahit , 1999 (66,90,46) "
+studentc = " Yuksel , 1998 (55,66,12) "
+print("++++++++++++++")
+input(studenta)
+print(studenta)
+print("++++++++++++++")
+input(studentb)
+print(studentb)
+print("++++++++++++++")
+input(studentc)
+print(studentc)
+print("++++++++++++++")
